6042841816f38753e6fefb9e9b18304a29705ade
sifanaks <sifanakollamkudy@gmail.com>
2023-03-08 17:44:58 +0530

    Filter and Add Product

--- ecommerce/src/component/Header.js
+++ ecommerce/src/component/Header.js
@@ -3,8 +3,11 @@ import {Nav,Navbar,} from 'react-bootstrap';
 import images from "../../src/images/images.jpg"
 import Login from './LoginModal';
 import { useState } from 'react';
+import AddProduct from './AddProductModal';
+import axios from 'axios';
 function Header() {
     const [show, setShow] = useState(false);
+    const [showProduct, setShowProduct] = useState(false);
   return (
     <>
       <Navbar bg="dark" variant="dark">
@@ -20,11 +23,12 @@ function Header() {
           </Navbar.Brand>
           <Nav className="justify-content-end">
             <Nav.Link onClick={()=>{setShow(true)}}>Login</Nav.Link>
-            <Nav.Link href="#features">Add product</Nav.Link>
+            <Nav.Link onClick={()=>{setShowProduct(true)}}>Add product</Nav.Link>
             
           </Nav>
         </Container>
         <Login show={show} setShow={setShow}/>
+        <AddProduct show={showProduct} setShow={setShowProduct}/>
       </Navbar>
     </>
   );
