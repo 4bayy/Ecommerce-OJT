038c9eab48b51df55c27e997461106a8f92a0861
sifanaks <sifanakollamkudy@gmail.com>
2023-03-14 10:56:14 +0530

    template

--- ecommerce/src/component/Header.js
+++ ecommerce/src/component/Header.js
@@ -1,71 +1,63 @@
-import Container from 'react-bootstrap/Container';
-import { Nav, Navbar } from 'react-bootstrap';
-import images from '../../src/images/images.jpg';
-import Login from './LoginModal';
-import { useState } from 'react';
-import AddProduct from './AddProductModal';
-import axios from 'axios';
-import { BsFillCartFill } from 'react-icons/bs';
-import { useNavigate } from 'react-router-dom';
-
+import Container from "react-bootstrap/Container";
+import { Nav, Navbar } from "react-bootstrap";
+import images from "../../src/images/images.jpg";
+import Login from "./LoginModal";
+import { useState } from "react";
+import AddProduct from "./AddProductModal";
+import { ToastContainer, Toast, toast } from "react-toastify";
+import { BsCart4} from 'react-icons/bs';
+import axios from "axios";
+import { BsFillCartFill } from "react-icons/bs";
+import "jquery/dist/jquery.slim.min.js";
+import "popper.js/dist/umd/popper.min.js";
+import "bootstrap/dist/js/bootstrap.min.js";
 function Header() {
-
-   const navigate =useNavigate();
-    const [show, setShow] = useState(false);
-    const [showProduct, setShowProduct] = useState(false);
-    const logOut = () => {
-        console.log(localStorage.token);
-        localStorage.clear();
-    };
-    const checkLogin = () => {
-        if (localStorage.token) {
-            setShowProduct(true);
-        } else {
-            setShow(true);
-        }
-    };
-    return (
-        <>
-            <Navbar bg="dark" variant="dark" fixed="top">
-                <Container>
-                    <Navbar.Brand href="#home">
-                        <img
-                            src={images}
-                            width="40"
-                            height="40"
-                            className="d-inline-block align-top"
-                            alt="Logo"
-                            style={{ borderRadius: '50%' }}
-                        />
-                    </Navbar.Brand>
-                    <Nav className="justify-content-end">
-                        {localStorage.token ? (
-                            <div style={{ display: 'flex' }}>
-                                <Nav.Link onClick={() => logOut()}>
-                                    Logout
-                                </Nav.Link>
-                                <Nav.Link onClick={()=>navigate(`/cartpage`)}>
-                                    <BsFillCartFill></BsFillCartFill>
-                                </Nav.Link>
-                                <span 
-                                style={{color:"red" ,
-                                 }}>Count</span>
-                            </div>
-                        ) : (
-                            <Nav.Link onClick={() => setShow(true)}>
-                                Login
-                            </Nav.Link>
-                        )}
-                        <Nav.Link onClick={() => checkLogin()}>
-                            Add product
-                        </Nav.Link>
-                    </Nav>
-                </Container>
-                <Login show={show} setShow={setShow} />
-                <AddProduct show={showProduct} setShow={setShowProduct} />
-            </Navbar>
-        </>
-    );
+  const [show, setShow] = useState(false);
+  const [showProduct, setShowProduct] = useState(false);
+  const logOut = () => {
+    console.log(localStorage.token);
+    localStorage.clear();
+    toast.success("successfully logged out", {
+      position: "top-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      theme: "light",
+    });
+    window.location.reload(false);
+  };
+  const checkLogin = () => {
+    if (localStorage.token) {
+      setShowProduct(true);
+    } else {
+      setShow(true);
+    }
+  };
+  return (
+    <>
+    <ToastContainer />
+    <nav class="navbar navbar-expand-lg navbar-light bg-white w-100 navigation" id="navbar">
+        <Container>
+          <Navbar.Brand href="#home" className="font-weight-bold">
+            Home
+          </Navbar.Brand>
+          <Nav className="justify-content-end font-weight-bold">
+          <Nav.Link onClick={() => checkLogin()}>Add product</Nav.Link>
+            {localStorage.token ? (
+              <div style ={{display:"flex"}}>
+              <Nav.Link onClick= {()=> logOut()}>Logout</Nav.Link>
+              <Nav.Link ><BsFillCartFill></BsFillCartFill></Nav.Link>
+              </div>
+            ) : (
+              <Nav.Link onClick={() => setShow(true)}>Login</Nav.Link>
+            )}
+            
+          </Nav>
+        </Container>
+        <Login show={show} setShow={setShow} />
+        <AddProduct show={showProduct} setShow={setShowProduct} />
+      </nav>
+    </>
+  );
 }
 
 export default Header;
