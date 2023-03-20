b8392d4b45077ffba8f74aec4e474f382207b57c
sifanaks <sifanakollamkudy@gmail.com>
2023-03-20 10:09:41 +0530

    cart-change

--- ecommerce/src/components/card/ProductCard.js
+++ ecommerce/src/components/card/ProductCard.js
@@ -65,7 +65,7 @@ function ProductCard(props) {
                         <div class="btn-group">
                             <div class="btn ">
                                 <a onClick={()=>checkLogin(props.item)}>
-                                    ADD TO<i class="tf-ion-android-cart"></i>
+                                    ADD TO <i class="tf-ion-android-cart"></i>
                                 </a>
                                 <hr></hr>
                             </div>
